ef0793817be83686b4859d0418110a1f3f21af57
Juan David <juandapv995@gmail.com>
2026-03-31 23:15:43 -0500

    Private Subnets

--- modules/subnets/subnets.tf
+++ modules/subnets/subnets.tf
@@ -18,3 +18,23 @@ resource "aws_subnet" "Public-subnet-2" {
     Description = "Second public subnet for custom VPC in Project1"
   }
 }
+
+resource "aws_subnet" "Private-subnet-1" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block = var.range3
+  availability_zone = var.zones1
+  tags = {
+    Name = "Private-subnet-1"
+    Description = "First private subnet for custom VPC in Project1"
+  }
+}
+
+resource "aws_subnet" "Private-subnet-2" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block = var.range4
+  availability_zone = var.zones2
+  tags = {
+    Name = "Private-subnet-2"
+    Description = "Second private subnet for custom VPC in Project1"
+  }
+}
--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -10,6 +10,18 @@ variable "range2" {
     default     = "10.0.20.0/24"
 }
 
+variable "range3" {
+    description = "The CIDR block for the first private subnet."
+    type        = string
+    default     = "10.0.100.0/24"
+}
+
+variable "range4" {
+    description = "The CIDR block for the second private subnet."
+    type        = string
+    default     = "10.0.200.0/24"
+}
+
 variable "zones1" {
     description = "The availability zone for the first public subnet."
     type        = string
