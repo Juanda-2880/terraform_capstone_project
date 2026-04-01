70260aab635156210d9dd337987762859f945cd1
Juan David <juandapv995@gmail.com>
2026-04-01 15:00:28 -0500

    Uptaded Route Tables

--- modules/route_table/route_table.tf
+++ modules/route_table/route_table.tf
@@ -25,3 +25,37 @@ resource "aws_route_table_association" "public_subnet2" {
   route_table_id = aws_route_table.public_route_table.id
   subnet_id      = aws_subnet.Public_Subnet2.id
 }
+
+resource "aws_route_table" "Private_RT_1" {
+    vpc_id = aws_vpc.custom_vpc.id
+    route {
+        cidr_block = var.cidr1
+        nat_gateway_id = aws_nat_gateway.NATGW1.id
+    }
+    route {
+        cidr_block = var.cidr2
+        gateway_id = "local"
+    }
+  tags = {
+    Name = "Private-RT-1"
+    Description = "Route Table for private subnet 1 in Project1"
+  }
+  
+}
+
+resource "aws_route_table" "Private_RT_2" {
+    vpc_id = aws_vpc.custom_vpc.id
+    route {
+        cidr_block = var.cidr1
+        nat_gateway_id = aws_nat_gateway.NATGW2.id
+    }
+    route {
+        cidr_block = var.cidr2
+        gateway_id = "local"
+    }
+  tags = {
+    Name = "Private-RT-2"
+    Description = "Route Table for private subnet 2 in Project1"
+  }
+  
+}
