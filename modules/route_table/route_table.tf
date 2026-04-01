59feb6e2a3844feb153950353f0433aaa3023bf2
Juan David <juandapv995@gmail.com>
2026-03-31 23:13:06 -0500

    Association Route Table

--- modules/route_table/route_table.tf
+++ modules/route_table/route_table.tf
@@ -13,3 +13,15 @@ resource "aws_default_route_table" "Public-RT" {
     Description = "Default Route Table for custom VPC in Project1"
   }
 }
+
+resource "aws_route_table_association" "public_subnet1" {
+  depends_on     = [aws_subnet.Public_Subnet1]
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.Public_Subnet1.id
+}
+
+resource "aws_route_table_association" "public_subnet2" {
+  depends_on     = [aws_subnet.Public_Subnet2]
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.Public_Subnet2.id
+}
